601f59c1a89f1940adea3f92b2928277cbcb3b74
alban-okoby <albanokoby225@gmail.com>
2024-03-06 23:03:35 +0000

    All

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,6 @@
-export default async function fetchData(boolean) {
-  return new Promise((resolve, reject) => {
-    const object = {
-      status: 200,
-      body: 'Success',
-    };
-    if (boolean === true) {
-      resolve(object);
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
