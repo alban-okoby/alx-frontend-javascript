20af904a507fed35b9707eef0520da2837f42bc3
alban-okoby <albanokoby225@gmail.com>
2024-03-07 16:04:33 +0000

    Airport issue

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,11 +1,17 @@
 export default class Airport {
   constructor(name, code) {
-    this._name = name;
-    this._code = code;
+    this._name = this.validateString(name, 'Name');
+    this._code = this.validateString(code, 'Code');
   }
 
   get [Symbol.toStringTag]() {
     return `${this._code}`;
   }
-	
+
+  validateString(value, label) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${label} must be a string`);
+    }
+    return value;
+  }
 }
