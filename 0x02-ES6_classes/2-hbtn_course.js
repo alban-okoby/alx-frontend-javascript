d7273646325fd24816094af00ea2497a9f0ab977
alban-okoby <albanokoby225@gmail.com>
2024-03-07 14:35:56 +0000

    A course, getter & setter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = typeof name === 'string' ? name : '';
+    this._length = typeof length === 'number' ? length : 0;
+    this._students = Array.isArray(students) ? students : [];
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      console.error('Name should be a string.');
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength === 'number') {
+      this._length = newLength;
+    } else {
+      console.error('Length should be a number.');
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents)) {
+      this._students = newStudents;
+    } else {
+      console.error('Students should be an array of strings.');
+    }
+  }
+}
+
+export default HolbertonCourse;
