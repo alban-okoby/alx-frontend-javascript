1d8046dd49ba09bfa1a8f406dbaee6c19f796357
alban-okoby <albanokoby225@gmail.com>
2024-03-07 15:01:21 +0000

    Airport

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,12 +1,6 @@
 import Building from './5-building.js';
 
-export default class SkyHighBuilding extends Building {
-
-   /**
-   * Constructor for SkyHighBuilding class
-   * @param {Number} sqft - Square footage of the building
-   * @param {Number} floors - Number of floors in the building
-   */
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
@@ -17,6 +11,8 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
+
+export default SkyHighBuilding;
