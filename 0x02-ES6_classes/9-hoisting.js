988146702174656f917f67aee32d4224487cea18
alban-okoby <albanokoby225@gmail.com>
2024-03-07 15:41:45 +0000

    HolbertonClass  issue ❌

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,9 @@
 export class HolbertonClass {
+  /**
+   * Constructor for HolbertonClass
+   * @param {number} year - The year of the class
+   * @param {string} location - The location of the class
+   */
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -13,7 +18,16 @@ export class HolbertonClass {
   }
 }
 
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
 export class StudentHolberton {
+  /**
+   * Constructor for StudentHolberton
+   * @param {string} firstName - The first name of the student
+   * @param {string} lastName - The last name of the student
+   * @param {HolbertonClass} holbertonClass - The HolbertonClass instance associated with the student
+   */
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -33,13 +47,12 @@ export class StudentHolberton {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
