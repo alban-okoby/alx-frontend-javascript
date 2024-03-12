2b5d2384351ec285029fd8fdbbf15f44f8210fc3
alban-okoby <albanokoby225@gmail.com>
2024-03-12 11:16:08 +0000

    Basic list of objects

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,17 +1,11 @@
 export default class Airport {
   constructor(name, code) {
-    this._name = this.validateString(name, 'Name');
-    this._code = this.validateString(code, 'Code');
+    this._name = name;
+    this._code = code;
   }
 
   get [Symbol.toStringTag]() {
     return `${this._code}`;
   }
-
-  validateString(value, label) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${label} must be a string`);
-    }
-    return value;
-  }
+	
 }
